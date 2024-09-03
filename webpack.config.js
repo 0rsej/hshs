e789511b64f0ac07aec5cb4ee95873116f4c7de0
0rsje <hassen23277@gmail.com>
2024-09-03 11:59:22 +0300

    Initial commit

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,37 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './app.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
-  },
-  mode: 'production'
+    entry: './app.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './index.html',
+            filename: 'index.html'
+        }),
+        new HtmlWebpackPlugin({
+            template: './card-info.html',
+            filename: 'card-info.html'
+        }),
+        new HtmlWebpackPlugin({
+            template: './confirmation.html',
+            filename: 'confirmation.html'
+        }),
+        new HtmlWebpackPlugin({
+            template: './new-page.html',
+            filename: 'new-page.html'
+        }),
+        new HtmlWebpackPlugin({
+            template: './payment-summary.html',
+            filename: 'payment-summary.html'
+        })
+    ],
+    devServer: {
+        static: path.join(__dirname, 'dist'),
+        compress: true,
+        port: 9000,
+    },
 };
